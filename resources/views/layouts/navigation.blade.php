e9af07fe5b63ccb73254721855880e281c5203e0
Julio Filgueiras <julio_filgueiras@live.com>
2021-07-23 00:50:17 -0300

    Show subscriptions details

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('subscriptions.premium')" :active="request()->routeIs('subscriptions.premium')">
+                        {{ __('Premmium') }}
+                    </x-nav-link>
                 </div>
             </div>
 
